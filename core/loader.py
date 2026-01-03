6d20ee3ebd3d58cb087aa33f30a53cd6e06fcc04
Calvin <603992850@qq.com>
2026-01-03 13:13:51 +0800

    feat: add avoiding not files or file is none but pass logic

--- core/loader.py
+++ core/loader.py
@@ -14,12 +14,17 @@ class FileLoader():
         print(f"[FileLoader] Scanning directory: {logdir}")
         print(f"[FileLoader] Found files: {file_list}")
 
+        # 避免无文件造成的异常通过
         if not file_list:
             print(f"[WARN] No .log files found in {logdir}")
-            return
+            raise FileNotFoundError(f"No .log files found in {logdir}. Did the Runner fail?")
 
         for filepath in file_list:
             print(f"[FileLoader] Reading: {filepath}")
+
+            # 避免空文件造成的异常通过
+            if os.path.getsize(filepath) == 0:
+                raise ValueError(f"Log file is empty: {filepath}. Test execution might have failed silently.")
             with open(filepath,"r",encoding="utf-8",errors="ignore") as f:
                 for line in f:
                     yield line
