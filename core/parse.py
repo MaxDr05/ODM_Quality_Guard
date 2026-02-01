59070c03f8b22d5c319988d6c91eddd465c22859
Calvin <603992850@qq.com>
2026-02-01 16:59:09 +0800

    refactor:update find logic-use regex instead in

--- core/parse.py
+++ core/parse.py
@@ -1,5 +1,6 @@
 from .loader import FileLoader
 from typing import Generator
+import re
 
 class LogParser():
     def __init__(self,rules:list = None):
@@ -22,18 +23,42 @@ class LogParser():
                 "category": "Network"
             }
         ]
+
+        patterns_to_compile = []
+        self.pattern_map = {}
+        for rule in self.rules:
+            # 防御性编程：去除首尾空格，防止配置失误
+            origin_pattern = rule["pattern"].strip()
+
+            # 只支持“纯文本匹配”，不支持正则配置（这是为了安全的取舍）
+            escaped_pattern = re.escape(origin_pattern)
+
+            patterns_to_compile.append(escaped_pattern)
+            self.pattern_map[origin_pattern] = rule
+
+        patterns_to_compile.sort(key=len, reverse=True)
+        full_regex_str = "|".join(patterns_to_compile)
+        self.regex_obj = re.compile(full_regex_str)
+
+
     def parse(self,filepath) -> Generator:
         log_lines = FileLoader.load(logdir=filepath)
         for num,line in enumerate(log_lines):
             line = line.strip()
-            for rule in self.rules:
-                if rule["pattern"] in line.strip():
-                    mes_dict = {
+            # 使用编译好的对象进行搜索
+            match = self.regex_obj.search(line)
+            if match:
+                # 因为使用了 re.escape，match.group() 拿到的就是原文
+                matched_text = match.group()
+                rule = self.pattern_map.get(matched_text)
+
+                # 双重保险：防止极端的 key 不存在情况
+                if rule:
+                    yield {
                         "line_content": line,
                         "level": rule["level"],
                         "category": rule["category"],
                         "line_no": num + 1,
                         "pattern": rule["pattern"]
                     }
-                    yield mes_dict
 
